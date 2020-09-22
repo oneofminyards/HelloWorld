fd6516a4beb7a77ac365136465ed78556313396d
oneofminyards <71725779+oneofminyards@users.noreply.github.com>
2020-09-22 21:04:37 +0300

    Initial commit

---
+++ Assets/Scripts/MagicNumbers.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicNumbers : MonoBehaviour
+   
+{
+    public int min = 1;
+    public int max = 1000;
+    int guess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //min = 1;
+        //max = 1000;
+        print(" Загадайте число от " + min + " до " +  max);
+        
+
+        UpdateGuess();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            UpdateGuess();
+        }
+       if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            UpdateGuess();
+        }
+       if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print(" I'm a wizard, Harry ");
+            Start();
+        }
+    }
+    
+    void UpdateGuess()
+    {
+        guess = (min + max) / 2;
+        print(" Ваше число " + guess + " ? ");
+
+    }
+}
---
+++ Assets/Scripts/NumbersTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumbersTest : MonoBehaviour
+{
+    int number; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // number = number + 1;
+        //number += 1; // -= 1
+        number++; //number--
+
+        print(number);
+    }
+}
